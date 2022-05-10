70dad9f62003135cf14ad2c160a7a59a07155ffe
Krupa Bhavsar <krups4444@gmail.com>
2022-05-10 11:39:05 +0530

    navbar

--- young-india/src/components/Navbar/Navbar.js
+++ young-india/src/components/Navbar/Navbar.js
@@ -1,25 +1,32 @@
 import React from 'react'
+import img1 from './images/YoungIN1 1.png';
 
 export default function Navbar() {
   return (
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
+    <img src="img1" width="30" height="30" alt=""/>
   </a>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Home </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Project</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Gallery</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link disabled" href="#">Event</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact us</a>
                     </li>
                 </ul>
             </div>
